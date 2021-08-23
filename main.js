0ab4fb0dd01e69e975b59e188f961bb14255ece0
hamzah-sajid-code <82176013+hamzah-sajid-code@users.noreply.github.com>
2021-08-23 19:40:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+var img = '';
+var status = '';
+var arrayOfResults = [];
+
+
+var CSS_COLOR_NAMES = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"];
+var video = '';
+
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(640, 420)
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(640, 420);
+    video.hide();
+    setTimeout(() => {
+        objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+    }, 5000);
+
+}
+
+function modelLoaded() {
+    console.log('Successfully Loaded Coco SSD');
+}
+
+
+
+function draw() {
+    image(video, 0, 0, 640, 420);
+    setTimeout(() => {
+        objectDetector.detect(video, gotResults);
+    }, 5000);
+    for (i = 0; arrayOfResults.length > i; i++) {
+        console.log(arrayOfResults[i].label)
+        document.getElementById('status').innerHTML = arrayOfResults[i].label;
+    }
+
+}
+
+function gotResults(error, results) {
+    console.log('Got Results')
+
+    if (results.length > 0) {
+        // console.log(results);
+        arrayOfResults = results;
+        console.log(arrayOfResults)
+    }else{
+        console.log(error)
+    }
+    
+}
+
+// not needed codes
+
+// colorindex = Math.floor(Math.random()*CSS_COLOR_NAMES.length);
+// console.log(CSS_COLOR_NAMES[colorindex])
